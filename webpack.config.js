89923a1fe7a3929af73a12412a5afc84421708dd
gknano <gkdvps@gmail.com>
2021-02-24 15:34:15 +0300

    fixed bag routing(cannot get page)

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js",
-    publicPath: "/",
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/',
   },
   resolve: {
-    modules: [path.join(__dirname, "src"), "node_modules"],
+    modules: [path.join(__dirname, 'src'), 'node_modules'],
     alias: {
-      react: path.join(__dirname, "node_modules", "react"),
+      react: path.join(__dirname, 'node_modules', 'react'),
     },
   },
   module: {
@@ -20,25 +20,28 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
       {
         test: /\.css$/,
         use: [
           {
-            loader: "style-loader",
+            loader: 'style-loader',
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
           },
         ],
       },
     ],
   },
+  devServer: {
+    historyApiFallback: true,
+  },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
   ],
-};
+}
